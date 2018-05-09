326a85c8a17a0537195a880bfa18b1d6eb15ab3f
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-09 21:27:49 +0100

    basic structure of the project

---
+++ app/src/main/java/com/example/android/kotlinnotepad/crud/MainActivity.java
@@ -0,0 +1,4 @@
+package com.example.android.kotlinnotepad.crud;
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/example/android/kotlinnotepad/crud/NotesApplication.java
@@ -0,0 +1,8 @@
+package com.example.android.kotlinnotepad.crud;
+
+/**
+ * Created by e6420 on 5/9/2018.
+ */
+
+public class NotesApplication {
+}
