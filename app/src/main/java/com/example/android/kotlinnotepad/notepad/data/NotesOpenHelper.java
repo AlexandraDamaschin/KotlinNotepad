6b30eb75e945df70cbf170be440bdcbcf3809b13
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-12 19:48:51 +0100

    database

--- app/src/main/java/com/example/android/kotlinnotepad/notepad/data/NotesOpenHelper.java
+++ app/src/main/java/com/example/android/kotlinnotepad/notepad/data/NotesOpenHelper.java
@@ -1,5 +1,26 @@
 package com.example.android.kotlinnotepad.notepad.data;
 
 
-public class NotesOpenHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.android.kotlinnotepad.notepad.data.NotesContract.SQL_CREATE_ENTRIES;
+import static com.example.android.kotlinnotepad.notepad.data.NotesContract.SQL_DELETE_ENTRIES;
+
+public class NotesOpenHelper extends SQLiteOpenHelper {
+    public NotesOpenHelper(Context context) {
+        super(context, "notes.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
 }
