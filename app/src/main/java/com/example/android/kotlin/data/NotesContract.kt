3dfc768a16b1f45997810adc1c55888140925c67
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-13 15:08:48 +0100

    SQL_CREATE_ENTRIES

--- app/src/main/java/com/example/android/kotlin/data/NotesContract.kt
+++ app/src/main/java/com/example/android/kotlin/data/NotesContract.kt
@@ -1,8 +1,10 @@
 package com.example.android.kotlin.data
 
-import android.provider.BaseColumns
-
 import com.example.android.kotlin.data.NotesContract.NoteTable.CREATED_AT
+import com.example.android.kotlin.data.NotesContract.NoteTable.IS_PINNED
+import com.example.android.kotlin.data.NotesContract.NoteTable.TEXT
+import com.example.android.kotlin.data.NotesContract.NoteTable.UPDATED_AT
+import com.example.android.kotlin.data.NotesContract.NoteTable._ID
 import com.example.android.kotlin.data.NotesContract.NoteTable._TABLE_NAME
 
 object NotesContract {
@@ -17,12 +19,13 @@ object NotesContract {
 
     }
 
-    val SQL_CREATE_ENTRIES = "CREATE TABLE " + _TABLE_NAME + " (" +
-            NoteTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-            NoteTable.TEXT + " TEXT, " +
-            NoteTable.IS_PINNED + " INTEGER, " +
+    val SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + _TABLE_NAME + " (" +
+            _ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+            TEXT + " TEXT, " +
+            IS_PINNED + " INTEGER, " +
             CREATED_AT + " INTEGER, " +
-            NoteTable.UPDATED_AT + " INTEGER" +
+            UPDATED_AT + " INTEGER" +
             ")"
 
     val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + _TABLE_NAME
