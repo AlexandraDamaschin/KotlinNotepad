29dd0c7e427910ee5ad03d9633fe0afb1bb4ab65
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-13 14:37:52 +0100

    data class

--- app/src/main/java/com/example/android/kotlin/data/Note.kt
+++ app/src/main/java/com/example/android/kotlin/data/Note.kt
@@ -2,17 +2,17 @@ package com.example.android.kotlin.data
 
 import java.util.Date
 
-class Note {
+data class Note(
 
-    //variables needed
-    //get set id
-    var id = -1
-    //get and set text
-    var text: String? = null
-    //get and set is pinned
-    var isPinned = false
-    //get and set date
-    var createdAt = Date()
-    //get and set update date
-    var updatedAt: Date? = null
-}
+        //variables needed
+        //get set id
+        var id: Int = -1,
+        //get and set text
+        var text: String? = null,
+        //get and set is pinned
+        var isPinned: Boolean = false,
+        //get and set date
+        var createdAt: Date = Date(),
+        //get and set update date
+        var updatedAt: Date? = null
+)
