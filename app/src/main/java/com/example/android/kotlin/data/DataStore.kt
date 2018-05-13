d10e4e6663a5a3149b42f6307a68b55d53e7dbdc
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-13 15:19:58 +0100

    convert dataStore to katolin

---
+++ app/src/main/java/com/example/android/kotlin/data/DataStore.kt
@@ -0,0 +1,22 @@
+package com.example.android.kotlin.data
+
+import android.content.Context
+
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+object DataStore {
+
+    val EXEC: Executor = Executors.newSingleThreadExecutor()
+
+    lateinit var notes: NoteDatabase
+        private set
+
+    fun init(context: Context) {
+        notes = NoteDatabase(context)
+    }
+
+    fun execute(runnable: Runnable) {
+        EXEC.execute(runnable)
+    }
+}
